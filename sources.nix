6938573829f0ca86f83e50d206cb0c486cefa5f4
cordelia76544 <83532024+cordelia76544@users.noreply.github.com>
2026-09-10 02:57:36 +0000

    chore(update): bump zedg to latest upstream release

--- sources.nix
+++ sources.nix
@@ -1,8 +1,8 @@
 {
-  version = "1.18.1.1";
+  version = "1.19.2";
 
   x86_64-linux = {
-    url = "https://github.com/x6nux/zed-globalization/releases/download/v1.18.1.1/zedg-zh-cn-linux-x86_64-v1.18.1.tar.gz";
-    hash = "sha256-M8tl9uWFKf4lTz0US4QKDBja2p/UP4Q8kPG2iCYi4Rs=";
+    url = "https://github.com/x6nux/zed-globalization/releases/download/v1.19.2/zedg-zh-cn-linux-x86_64-v1.19.2.tar.gz";
+    hash = "sha256-uSG1yXpof91yKz3EZZAJM7LrZQGlPvCckLZjhOHuSt8=";
   };
 }
